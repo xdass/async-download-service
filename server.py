910702b72e413f8358f4fba6f41b1aca99596c52
dass <420600base>
2019-05-20 16:24:29 +0500

    Add options for script

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 import os
+import argparse
 import logging
 import asyncio
-import aiohttp
 from asyncio.subprocess import create_subprocess_shell, PIPE
+import aiohttp
 from aiohttp import web
 import aiofiles
 
@@ -14,11 +15,8 @@ ch = logging.StreamHandler()
 logger.addHandler(ch)
 
 
-def check_path():
-    pass
-
-
-async def archivate(request):
+async def archivate(request, use_delay):
+    print(use_delay)
     archive_hash_idx = 2
     archive_hash = request.path.split('/')[archive_hash_idx]
     path_to_images = os.path.join(os.getcwd(), 'test_photos', archive_hash)
@@ -32,21 +30,21 @@ async def archivate(request):
 
     process = await create_subprocess_shell(f'zip -j -r - {os.path.join(path_to_images, "*.*")}', stdout=PIPE, stderr=PIPE)
 
-    while True:
-        try:
+    try:
+        while True:
             archive_chunk = await process.stdout.read(256)
             if archive_chunk:
-                await asyncio.sleep(10)
-                print(archive_chunk)
+                if use_delay:
+                    await asyncio.sleep(30)
                 logger.info(f'Sending archive chunk ...')
                 await response.write(archive_chunk)
             else:
                 return response
-        except Exception as e:
-            process.send_signal(9)
-            raise
-        finally:
-            response.force_close()
+    except asyncio.CancelledError:
+        process.send_signal(19)
+        raise
+    finally:
+        response.force_close()
 
 
 async def handle_index_page(request):
@@ -56,6 +54,17 @@ async def handle_index_page(request):
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Microservice for download files')
+    parser.add_argument('--logging', help='Enabled or Disabled logging(By default is Enabled)', type=bool, default=False)
+    parser.add_argument('--use_delay', help='Enabled delay before send chunk(for debuging)', type=bool, default=False)
+    parser.add_argument('images', metavar='i', help='Path to images folder')
+    args = parser.parse_args()
+    logger_state = args.logging
+    use_delay = args.use_delay
+    images_folder = args.images
+    logger.disabled = logger_state
+    full_path = os.path.join(images_folder, '{archive_hash}/')
+
     app = web.Application()
     app.add_routes([
         web.get('/', handle_index_page),
